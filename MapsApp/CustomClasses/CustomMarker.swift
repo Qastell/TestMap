bdb52041087af24d1fc4b321acfd7c678a6d787c
Kirill Romanenko <kirrom@bk.ru>
2020-11-16 19:10:18 +0800

    icon -> iconView

--- MapsApp/CustomClasses/CustomMarker.swift
+++ MapsApp/CustomClasses/CustomMarker.swift
@@ -15,10 +15,36 @@ class CustomMarker: GMSMarker {
     private var numberOfVacancy = 0
     private var salary = 0
     
+    private var pinIsShowing = false {
+        didSet {
+            if pinIsShowing == true {
+                infoIsShowing = false
+                pointIsShowing = false
+            }
+        }
+    }
+    
+    private var pointIsShowing = false {
+        didSet {
+            if pointIsShowing == true {
+                pinIsShowing = false
+                infoIsShowing = false
+            }
+        }
+    }
+    
+    private var infoIsShowing = false {
+        didSet {
+            if infoIsShowing == true {
+                pinIsShowing = false
+                pointIsShowing = false
+            }
+        }
+    }
+    
     override init() {
         super.init()
         MapViewController.delegates.add(self)
-        tracksViewChanges = true
     }
     
     func postVacancy(number: Int, salary: Int) {
@@ -30,59 +56,110 @@ class CustomMarker: GMSMarker {
 
 extension CustomMarker: MapViewDelegate {
     func changeImageOfMarker(zoom: Float) {
-        if zoom < 13.5 {
+        
+        if zoom < 13.5, pointIsShowing == false {
+            pointIsShowing = true
+
             let blueView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
             blueView.backgroundColor = #colorLiteral(red: 0.3607843137, green: 0.6745098039, blue: 1, alpha: 1)
             blueView.layer.cornerRadius = blueView.frame.width/2
             blueView.layer.borderColor = UIColor.black.cgColor
             blueView.layer.borderWidth = 1
-            
-            icon = blueView.asImage()
-        } else if zoom > 13.5, zoom <= 15 {
+
+            iconView = blueView
+        } else if zoom > 13.5, zoom <= 15, pinIsShowing == false {
+            pinIsShowing = true
+
             if numberOfVacancy == 0 || numberOfVacancy == 1 {
-                icon = UIImage(named: "pin")
+                iconView = drawPin()
             } else {
-                let image = textToImage(drawText: "\(numberOfVacancy)",
-                                        inImage: UIImage(named: "pinWithNumber")!,
-                                        font: UIFont.boldSystemFont(ofSize: 15))
-                icon = image
+                iconView = drawPinWithNumber(numberOfVacancy)
             }
-        } else if zoom > 15 {
+        } else if zoom > 15, infoIsShowing == false {
+            infoIsShowing = true
+
             if numberOfVacancy == 0 || numberOfVacancy == 1{
-                let image = textToImage(drawText: "\(salary) тыс.",
-                                        inImage: UIImage(named: "table")!,
-                                        font: UIFont.boldSystemFont(ofSize: 15))
-                icon = image
+                iconView = drawTable(salary: salary)
             } else {
-                let image = textToImage(drawText: "\(numberOfVacancy) от \(salary) тыс.",
-                                        inImage: UIImage(named: "bigTable")!,
-                                        font: UIFont.boldSystemFont(ofSize: 14))
-                icon = image
+                iconView = drawBigTable(salary: salary, vacancy: numberOfVacancy)
             }
         }
+        
     }
     
+    func drawBigTable(salary: Int, vacancy: Int) -> UIView {
+        let markerView = UIView(frame: CGRect(x: 0, y: 0, width: 110, height: 40))
+        let imageView = UIImageView(image: UIImage(named: "bigTable")!)
+        imageView.frame = markerView.frame
+        imageView.contentMode = .scaleAspectFit
+        imageView.center = markerView.center
+        
+        let label = UILabel()
+        let textH = label.font.lineHeight
+        label.text = "\(vacancy) от \(salary) тыс."
+        label.textAlignment = .center
+        label.frame = markerView.frame
+        label.center = markerView.center
+        label.frame.origin.y = label.frame.origin.y - textH/3
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        
+        markerView.addSubview(imageView)
+        markerView.addSubview(label)
+        
+        return markerView
+    }
     
-
-    private func textToImage(drawText text: String, inImage image: UIImage, font: UIFont) -> UIImage {
+    func drawTable(salary: Int) -> UIView {
+        let markerView = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 40))
+        let imageView = UIImageView(image: UIImage(named: "table")!)
+        imageView.frame = markerView.frame
+        imageView.contentMode = .scaleAspectFit
+        imageView.center = markerView.center
         
-        UIGraphicsBeginImageContext(image.size)
-        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        let label = UILabel()
+        let textH = label.font.lineHeight
+        label.text = "\(salary) тыс."
+        label.textAlignment = .center
+        label.frame = markerView.frame
+        label.center = markerView.center
+        label.frame.origin.y = label.frame.origin.y - textH/4
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 14)
         
-        let textStyle = NSMutableParagraphStyle()
-        textStyle.alignment = NSTextAlignment.center
-        let textColor = UIColor.white
-        let attributes=[
-            NSAttributedString.Key.font: font,
-            NSAttributedString.Key.paragraphStyle: textStyle,
-            NSAttributedString.Key.foregroundColor: textColor]
+        markerView.addSubview(imageView)
+        markerView.addSubview(label)
         
-        let textH = font.lineHeight
-        let textY = (image.size.height-textH)/2-(textH/4)-3
-        let textRect = CGRect(x: 0, y: textY, width: image.size.width, height: textH)
-        text.draw(in: textRect.integral, withAttributes: attributes)
-        guard let result = UIGraphicsGetImageFromCurrentImageContext() else { return UIImage() }
-        UIGraphicsEndImageContext()
-        return result
+        return markerView
+    }
+    
+    func drawPinWithNumber(_ numberOfVacancy: Int)->UIView {
+        let markerView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 40))
+        let imageView = UIImageView(image: UIImage(named: "pinWithNumber")!)
+        imageView.frame = markerView.frame
+        imageView.contentMode = .scaleAspectFit
+        imageView.center = markerView.center
+        
+        let label = UILabel()
+        let textH = label.font.lineHeight
+        label.text = "\(numberOfVacancy)"
+        label.textAlignment = .center
+        label.frame = markerView.frame
+        label.center = markerView.center
+        label.frame.origin.y = label.frame.origin.y - textH/5
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        markerView.addSubview(imageView)
+        markerView.addSubview(label)
+        
+        return markerView
+    }
+    
+    func drawPin() -> UIView {
+        let pinView = UIImageView(image: UIImage(named: "pin"))
+        pinView.frame = CGRect(x: 0, y: 0, width: 30, height: 40)
+        pinView.contentMode = .scaleAspectFit
+        return pinView
     }
 }
---
+++ MapsApp/Extensions/GMSMapView+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  GMSMapView+Extension.swift
+//  MapsApp
+//
+//  Created by Кирилл Романенко on 16.11.2020.
+//
+
+import Foundation
+import GoogleMaps
+
+extension GMSMapView {
+    
+    static var delegates = MultiDelegate<GMSMapViewDelegate>()
+    
+}
